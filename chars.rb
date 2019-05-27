3016f296c8ad86014c80cd94794917b13c98b94a
anjum-ahmed <aahmed@edge-ka.com>
2019-05-27 16:47:30 +0100

    characters

---
+++ chars.rb
@@ -0,0 +1,152 @@
+$idb = {
+	/bluey/i => {
+		name: 'Bluey',
+		gender: :female,
+		age: :kid,
+		negative: [:family_bluey]
+	},
+	/jean(-?luc)?/i => {
+		name: 'Jean-Luc',
+		gender: :male,
+		age: :kid,
+		negative: []
+	},
+	/bingo/i => {
+		name: 'Bingo',
+		gender: :male,
+		age: :kid,
+		negative: [:family_bluey]
+	},
+	/bandit/i => {
+		name: 'Bandit',
+		gender: :male,
+		age: :adult,
+		negative: [:family_bluey, :bluey_parents]
+	},
+	/chil?li/i => {
+		name: 'Mrs Chilli',
+		gender: :female,
+		age: :adult,
+		negative: [:family_bluey, :bluey_parents]
+	},
+	/lucky/i => {
+		name: 'Lucky',
+		gender: :male,
+		age: :kid,
+		negative: []
+	},
+	/indy/i => {
+		name: 'Indy',
+		gender: :female,
+		age: :kid,
+		negative: []
+	},
+	/coco/i => {
+		name: 'CoCo',
+		gender: :female,
+		age: :kid,
+		negative: []
+	},
+	/snickers/i => {
+		name: 'Snickers',
+		gender: :male,
+		age: :kid,
+		negative: []
+	},
+	/honey/i => {
+		name: 'Honey',
+		gender: :female,
+		age: :kid,
+		negative: []
+	},
+	/mac(kenzie)?/i => {
+		name: 'Mackenzie',
+		gender: :female,
+		age: :kid,
+		negative: []
+	},
+	/chloe/i => {
+		name: 'Chloe',
+		gender: :female,
+		age: :kid,
+		negative: []
+	},
+	/judo/i => {
+		name: 'Judo',
+		gender: :female,
+		age: :kid,
+		negative: []
+	},
+	/rusty?/i => {
+		name: 'Rusty',
+		gender: :male,
+		age: :kid,
+		negative: []
+	},
+	/muffin/i => {
+		name: 'Muffin',
+		gender: :female,
+		age: :kid,
+		negative: []
+	},
+	/socks/i => {
+		name: 'Socks',
+		gender: :female,
+		age: :kid,
+		negative: []
+	},
+	/pretzel/i => {
+		name: 'Pretzel',
+		gender: :male,
+		age: :kid,
+		negative: []
+	},
+	/bentley/i => {
+		name: 'Bentley',
+		gender: :female,
+		age: :kid,
+		negative: []
+	},
+	/missy/i => {
+		name: 'Missy',
+		gender: :female,
+		age: :kid,
+		negative: []
+	},
+	/rupert/i => {
+		name: 'Rupert',
+		gender: :male,
+		age: :kid,
+		negative: []
+	},
+	/juniper/i => {
+		name: 'Juniper',
+		gender: :female,
+		age: :kid,
+		negative: []
+	},
+	/buddy/i => {
+		name: 'Buddy',
+		gender: :male,
+		age: :kid,
+		negative: []
+	},
+	/(uncle )?strip/i => {
+		name: 'Uncle Strip',
+		gender: :male,
+		age: :adult,
+		negative: [:family_bluey, :cousin_parents]
+	},
+	/(aunt )?trix(ie)?/i => {
+		name: 'Aunt Trixie',
+		gender: :female,
+		age: :adult,
+		negative: [:family_bluey, :cousin_parents]
+	},
+	/buddy/i => {
+		name: 'Buddy',
+		gender: :male,
+		age: :kid,
+		negative: []
+	},
+}
